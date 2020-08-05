25abe98ef01dc6be445ee1156ab6291fc6d88708
Capucho <jcapucho7@gmail.com>
2020-08-05 17:46:05 +0100

    Update dependencies and solve freezes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use futures::executor::block_on;
+use wgpu::util::DeviceExt;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
 };
 
 #[repr(C)]
@@ -21,18 +21,18 @@ impl Vertex {
         wgpu::VertexBufferDescriptor {
             stride: mem::size_of::<Vertex>() as wgpu::BufferAddress,
             step_mode: wgpu::InputStepMode::Vertex,
-            attributes: &[
+            attributes: std::borrow::Cow::Borrowed(&[
                 wgpu::VertexAttributeDescriptor {
                     offset: 0,
                     shader_location: 0,
-                    format: wgpu::VertexFormat::Float3,
+                    format: wgpu::VertexFormat::Float4,
                 },
                 wgpu::VertexAttributeDescriptor {
-                    offset: mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    offset: mem::size_of::<[f32; 4]>() as wgpu::BufferAddress,
                     shader_location: 1,
-                    format: wgpu::VertexFormat::Float3,
+                    format: wgpu::VertexFormat::Float4,
                 },
-            ],
+            ]),
         }
     }
 }
@@ -65,7 +65,6 @@ fn vertex main() {
     f_position = 1.0 * v_position;
     f_color = 1.0 * color;
     gl_position = 1.0 * v_position;
-    return;
 }
 "#;
 
@@ -80,146 +79,161 @@ global position gl_position;
 
 fn fragment main() {
 	f_position = 1.0 * color;
-	return;
 }
 "#;
 
 fn main() {
-    env_logger::init();
-
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = winit::window::Window::new(&event_loop).unwrap();
 
     let size = window.inner_size();
 
-    let surface = wgpu::Surface::create(&window);
+    let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+    let surface = unsafe { instance.create_surface(&window) };
 
-    let adapter = block_on(wgpu::Adapter::request(
-        &wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::Default,
-            compatible_surface: Some(&surface),
+    let adapter = block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::Default,
+        // Request an adapter which can render to our surface
+        compatible_surface: Some(&surface),
+    }))
+    .unwrap();
+
+    let (device, queue) = block_on(adapter.request_device(
+        &wgpu::DeviceDescriptor {
+            features: wgpu::Features::empty(),
+            limits: wgpu::Limits::default(),
+            shader_validation: true,
         },
-        wgpu::BackendBit::PRIMARY, // Vulkan + Metal + DX12 + Browser WebGPU
+        None,
     ))
     .unwrap();
 
-    let (device, queue) = block_on(adapter.request_device(&wgpu::DeviceDescriptor {
-        extensions: wgpu::Extensions {
-            anisotropic_filtering: false,
-        },
-        limits: Default::default(),
-    }));
-
-    let sc_desc = wgpu::SwapChainDescriptor {
+    let mut sc_desc = wgpu::SwapChainDescriptor {
         usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
         format: wgpu::TextureFormat::Bgra8UnormSrgb,
         width: size.width,
         height: size.height,
-        present_mode: wgpu::PresentMode::Fifo,
+        present_mode: wgpu::PresentMode::Mailbox,
     };
     let mut swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
     let vs_spirv = rusty_shades::compile_to_spirv(VERT_SHADER).unwrap();
-    let vs_module = device.create_shader_module(&vs_spirv);
+    let vs_module = device.create_shader_module(wgpu::ShaderModuleSource::SpirV(
+        std::borrow::Cow::from(vs_spirv),
+    ));
     let fs_spirv = rusty_shades::compile_to_spirv(FRAG_SHADER).unwrap();
-    let fs_module = device.create_shader_module(&fs_spirv);
+    let fs_module = device.create_shader_module(wgpu::ShaderModuleSource::SpirV(
+        std::borrow::Cow::from(fs_spirv),
+    ));
 
     let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        bind_group_layouts: &[],
+        bind_group_layouts: std::borrow::Cow::Borrowed(&[]),
+        push_constant_ranges: std::borrow::Cow::Borrowed(&[]),
     });
 
-    let vertex_buffer =
-        device.create_buffer_with_data(bytemuck::cast_slice(VERTICES), wgpu::BufferUsage::VERTEX);
+    let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Vertex Buffer"),
+        contents: bytemuck::cast_slice(&VERTICES),
+        usage: wgpu::BufferUsage::VERTEX,
+    });
 
     let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
         layout: &render_pipeline_layout,
         vertex_stage: wgpu::ProgrammableStageDescriptor {
             module: &vs_module,
-            entry_point: "main",
+            entry_point: std::borrow::Cow::Borrowed("main"),
         },
         fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
             module: &fs_module,
-            entry_point: "main",
+            entry_point: std::borrow::Cow::Borrowed("main"),
         }),
-        rasterization_state: Some(wgpu::RasterizationStateDescriptor {
-            front_face: wgpu::FrontFace::Ccw,
-            cull_mode: wgpu::CullMode::Back,
-            depth_bias: 0,
-            depth_bias_slope_scale: 0.0,
-            depth_bias_clamp: 0.0,
-        }),
-        color_states: &[wgpu::ColorStateDescriptor {
+        rasterization_state: None,
+        color_states: std::borrow::Cow::Borrowed(&[wgpu::ColorStateDescriptor {
             format: sc_desc.format,
             color_blend: wgpu::BlendDescriptor::REPLACE,
             alpha_blend: wgpu::BlendDescriptor::REPLACE,
             write_mask: wgpu::ColorWrite::ALL,
-        }],
+        }]),
         primitive_topology: wgpu::PrimitiveTopology::TriangleList,
         depth_stencil_state: None,
         vertex_state: wgpu::VertexStateDescriptor {
             index_format: wgpu::IndexFormat::Uint16,
-            vertex_buffers: &[Vertex::desc()],
+            vertex_buffers: std::borrow::Cow::Borrowed(&[Vertex::desc()]),
         },
         sample_count: 1,
         sample_mask: !0,
         alpha_to_coverage_enabled: false,
     });
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-            WindowEvent::KeyboardInput { input, .. } => match input {
-                KeyboardInput {
-                    state: ElementState::Pressed,
-                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                    ..
-                } => *control_flow = ControlFlow::Exit,
+    event_loop.run(move |event, _, control_flow| {
+        let _ = (
+            &instance,
+            &adapter,
+            &vs_module,
+            &fs_module,
+            &render_pipeline_layout,
+        );
+
+        *control_flow = ControlFlow::Poll;
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::KeyboardInput { input, .. } => match input {
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
+                    _ => {}
+                },
+                WindowEvent::Resized(size) => {
+                    sc_desc.width = size.width;
+                    sc_desc.height = size.height;
+                    swap_chain = device.create_swap_chain(&surface, &sc_desc);
+                }
                 _ => {}
             },
+            Event::RedrawRequested(_) => {
+                let frame = swap_chain
+                    .get_current_frame()
+                    .expect("Timeout getting texture")
+                    .output;
 
-            _ => {}
-        },
-        Event::RedrawRequested(_) => {
-            let frame = swap_chain
-                .get_next_texture()
-                .expect("Timeout getting texture");
-
-            let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("Render Encoder"),
-            });
-
-            {
-                let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                    color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
-                        attachment: &frame.view,
-                        resolve_target: None,
-                        load_op: wgpu::LoadOp::Clear,
-                        store_op: wgpu::StoreOp::Store,
-                        clear_color: wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
-                        },
-                    }],
-                    depth_stencil_attachment: None,
+                let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                    label: Some(std::borrow::Cow::Borrowed("Render Encoder")),
                 });
 
-                render_pass.set_pipeline(&render_pipeline); // 2.
-                render_pass.set_vertex_buffer(0, &vertex_buffer, 0, 0);
-                render_pass.draw(0..3, 0..1); // 3.
-            }
+                {
+                    let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        color_attachments: std::borrow::Cow::Borrowed(&[
+                            wgpu::RenderPassColorAttachmentDescriptor {
+                                attachment: &frame.view,
+                                resolve_target: None,
+                                ops: wgpu::Operations {
+                                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                                        r: 0.1,
+                                        g: 0.2,
+                                        b: 0.3,
+                                        a: 1.0,
+                                    }),
+                                    store: true,
+                                },
+                            },
+                        ]),
+                        depth_stencil_attachment: None,
+                    });
 
-            queue.submit(&[encoder.finish()]);
+                    render_pass.set_pipeline(&render_pipeline);
+                    render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+                    render_pass.draw(0..3, 0..1);
+                }
+
+                queue.submit(Some(encoder.finish()));
+            }
+            _ => {}
         }
-        Event::MainEventsCleared => {
-            // RedrawRequested will only trigger once, unless we manually
-            // request it.
-            window.request_redraw();
-        }
-        _ => {}
     });
 }
